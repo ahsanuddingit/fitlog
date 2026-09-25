69e6026b44fea0b8f43ac301444148990d17b4ec
ahsanuddincv <mail.ahsanuddin@gmail.com>
2026-09-26 05:15:46 +0600

    muscleGroups update

--- src/components/WorkoutCard.tsx
+++ src/components/WorkoutCard.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 import { Workout } from '@/types/workout';
 
 const WorkoutCard = ({ data }: { data: Workout }) => {
-  const { id, name, duration, image, equipment, rating, caloriesBurned } = data;
+  const { id, name, duration, image, equipment, rating, caloriesBurned, muscleGroups } = data;
 
   return (
-    <Link href={`/${id}`} className="w-full">
+    <Link href={`/${id}`} className="w-full ">
       <div className="card bg-base-100 shadow-sm container mx-auto my-4 hover:border-gray-700 transition">
         <figure className="relative h-54 w-full">
           <Image
@@ -20,14 +20,26 @@ const WorkoutCard = ({ data }: { data: Workout }) => {
         </figure>
 
         <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p className="text-sm text-gray-400">{equipment}</p>
-            <div className="divider"></div>
+          <div className="flex flex-wrap gap-2 mb-2">
+            {muscleGroups?.map((group, index) => (
+              <span 
+                key={index} 
+                className="bg-[#ccff00] text-black font-extrabold uppercase text-xs px-3 py-1 rounded-full tracking-wider"
+              >
+                {group}
+              </span>
+            ))}
+          </div>
 
-          <div className="flex gap-2 mt-2">
-            <div className="badge badge-outline">{duration} min</div>
-            <div className="badge badge-outline">{rating} ⭐</div>
-            <div className="badge badge-outline">{caloriesBurned} kcal</div>
+          <h2 className="card-title font-bold text-xl uppercase">{name}</h2>
+          <p className="text-sm text-gray-400">{equipment}</p>
+
+          <div className="divider my-2"></div>
+
+          <div className="flex items-center justify-between text-sm text-gray-300 mt-2">
+            <span>⏱ {duration} min</span>
+            <span>🔥 {caloriesBurned} kcal</span>
+            <span>⭐ {rating}</span>
           </div>
         </div>
       </div>
